eef1f30b46d4d151f0739fe0832659c09d16d667
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-12-22 23:53:11 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@ cd /home/duythanh/projects/bots/angry-bird
 git pull
 python3 -m venv .venv
 source .venv/bin/activate
-pip install discord.py
-pip install asyncio
-pip install firebase-admin
-pip install g4f==0.1.9.3
+echo '[INFO] install discord.py'
+pip install discord.py --quiet
+echo '[INFO] install asyncio'
+pip install asyncio --quiet
+echo '[INFO] install firebase-admin'
+pip install firebase-admin --quiet
+echo '[INFO] install g4f'
+pip install g4f==0.1.9.3 --quiet
 python3 angry-bird.py
