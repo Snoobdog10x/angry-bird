41bcc4fd0992f7d251722bfc64d7ed3185f76a4b
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-12-22 23:20:43 +0700

    add install packages

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@ cd /home/duythanh/projects/bots/angry-bird
 git pull
 python3 -m venv .venv
 source .venv/bin/activate
+pip install discord.py
+pip install asyncio
+pip install firebase-admin
 pip install g4f==0.1.9.3
 python3 angry-bird.py
