ee4d34e7fa180d00177c05a6ccc866ad3bdd9e1b
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-12-25 18:50:36 +0700

    add local_redis package

---
+++ local_redis/redis_connection.py
@@ -0,0 +1,11 @@
+import json
+
+import redis
+import os
+
+sep = os.sep
+f = open(f"..{sep}redis_credential.json")
+data = json.load(f)
+PASSWORD = data["PASSWORD"]
+f.close()
+redis_instance = redis.Redis(host='192.168.2.19', port=6379, decode_responses=True, password=PASSWORD)
--- start.sh
+++ start.sh
@@ -11,4 +11,6 @@ echo '[INFO] install firebase-admin'
 pip install firebase-admin --quiet
 echo '[INFO] install g4f'
 pip install g4f==0.1.9.3 --quiet
+echo '[INFO] install local_redis'
+pip install local_redis --quiet
 python3 angry-bird.py
