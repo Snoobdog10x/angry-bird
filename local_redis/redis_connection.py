d805189ae32adfb6d459e7f62b8a1ff8bc5ebb69
Nguyen Duy Thanh <duythanh1565@gmail.com>
2024-01-01 15:27:57 +0700

    update redis credential

--- local_redis/redis_connection.py
+++ local_redis/redis_connection.py
@@ -4,8 +4,9 @@ import redis
 import os
 
 sep = os.sep
-f = open(f".{sep}redis_credential.json")
+f = open(f"..{sep}redis_credential.json")
 data = json.load(f)
 PASSWORD = data["PASSWORD"]
+IP = data["IP"]
 f.close()
-redis_instance = redis.Redis(host='192.168.2.19', port=6379, decode_responses=True, password=PASSWORD)
+redis_instance = redis.Redis(host=IP, port=6379, decode_responses=True, password=PASSWORD)
