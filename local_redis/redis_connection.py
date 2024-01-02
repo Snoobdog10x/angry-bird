74811f92b1e6e4a45b9515ac17ba527678de2ecf
Nguyen Duy Thanh <duythanh1565@gmail.com>
2024-01-02 22:38:05 +0700

    update angry-bird.service

--- local_redis/redis_connection.py
+++ local_redis/redis_connection.py
@@ -4,6 +4,3 @@ from firebase import *
 sep = os.sep
 IP, PASSWORD = credential_instance.get_redis_credential()
 redis_instance = redis.Redis(host=IP, port=6379, decode_responses=True, password=PASSWORD)
-
-redis_instance.set("thanh", "hihi")
-print(redis_instance.get("thanh"))
